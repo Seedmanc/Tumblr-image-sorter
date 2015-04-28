3fae94d2ca9f2157d328d22bb9dbb255e41e7531
Dmitry <seedmanc@yahoo.com>
2015-04-28 03:00:30 +0300

    TODO for no subfolders config

--- Opera native/animage-get.js
+++ Opera native/animage-get.js
@@ -118,7 +118,7 @@
 																		//debug enables error notification, shows im/export controls for aux dbs and save button
 																		//even if no tags were found, also disables cleanup (lag on tab close)
 // ==/Settings=====================================================
-
+												//TODO add support for no subfolders configuration
 var load,execute,loadAndExecute;load=function(a,b,c){var d;d=document.createElement("script"),d.setAttribute("src",a),b!=null&&d.addEventListener("load",b),c!=null&&d.addEventListener("error",c),document.body.appendChild(d);return d},execute=function(a){var b,c;typeof a=="function"?b="("+a+")();":b=a,c=document.createElement("script"),c.textContent=b,document.body.appendChild(c);return c},loadAndExecute=function(a,b){return load(a,function(){return execute(b)})};
 																		//external script loader function
 
